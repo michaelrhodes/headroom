7fbef6ec09b9b3c8dba806cd5fd749309f55857a
michaelrhodes <spam@michaelrhod.es>
2017-07-19 13:59:58 +1000

    Updates

--- headroom.js
+++ headroom.js
@@ -7,13 +7,12 @@ module.exports = headroom
 
 function headroom (el, view) {
   if (!(view = view || el.parentNode)) return
-  var sh = view.scrollHeight
   var eo = el.offsetHeight + el.offsetTop
   var dv = view.ownerDocument.defaultView
-  var end = view.scrollHeight - dv.innerHeight
+  var vh = view.scrollHeight - dv.innerHeight
   var y = 0, oy, hy = 0, t = 1.875
   return onscroll(view, function (ex, ey) {
-    oy = y, y = min(max(0, ey), end)
+    oy = y, y = min(max(0, ey), vh)
     if (y > oy && hy < eo) hy += min((y - oy) / t, eo - hy)
     if (y < oy && hy > 0) hy -= min((oy - y) / t, hy)
     el.style.transform = 'translate3d(0, -' + hy + 'px, 0)'
