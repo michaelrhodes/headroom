cb5d7599c8d970cd14b3adc780c473e5391b5bb8
michaelrhodes <spam@michaelrhod.es>
2017-07-20 00:20:28 +1000

    Updates

--- headroom.js
+++ headroom.js
@@ -10,11 +10,11 @@ function headroom (el, view) {
   var eo = el.offsetHeight + el.offsetTop
   var dv = view.ownerDocument.defaultView
   var vh = view.scrollHeight - dv.innerHeight
-  var y = 0, oy, hy = 0
+  var oy, hy = 0, y = 0, t = 1.333
   return onscroll(view, function (ex, ey) {
     oy = y, y = min(max(0, ey), vh)
-    if (y > oy && hy < eo) hy += min((y - oy), eo - hy)
-    if (y < oy && hy > 0) hy -= min((oy - y), hy)
+    if (y > oy && hy < eo) hy += min((y - oy) / t, eo - hy)
+    if (y < oy && hy > 0) hy -= min((oy - y) / t, hy)
     el.style.transform = 'translate3d(0, -' + hy + 'px, 0)'
   })
 }
